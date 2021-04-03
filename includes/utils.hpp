699c6d9b8bf8aef0e9e632bbec7c50940a811171
Sebastian Szczepański <s.szczepanski00@gmail.co>
2021-04-03 14:04:04 +0200

    Make Utils a header-only library

--- includes/utils.hpp
+++ includes/utils.hpp
@@ -13,15 +13,24 @@ namespace Utils {
    * @param x hexadecimal char
    * @return Byte 
    */
-  Byte hexCharToByte(char x);
+  inline Byte hexCharToByte(char x) {
+    if(x >= '0' && x <= '9') return x - '0';
+    if(x >= 'A' && x <= 'F') return x - 'A' + 10;
+    if(x >= 'a' && x <= 'f') return x - 'a' + 10;
+    return 0;
+  }
 
   /**
    * @brief Converts byte to hexadecimal char
    * 
-   * @param x Byte
-   * @return char 
+   * @param x byte
+   * @return Char 
    */
-  char byteToHexChar(Byte x);
+  inline char byteToHexChar(Byte x) {
+    if(x >= 0 && x <= 9) return x + '0';
+    if(x >= 10 && x <= 15) return (x - 10) + 'a';
+    return '0'; 
+  }
 
   /**
    * @brief Converts hexadecimal string to buffer
@@ -29,18 +38,28 @@ namespace Utils {
    * @param input c string 
    * @param inputLength length of the input string
    * @param output output buffer
-   * @return pointer to the last byte, nullptr if string is empty
+   * @return Pointer to the last byte, nullptr if string is empty
    */
-  Buffer hexStringToBuffer(const char *input, std::size_t inputLength, Buffer output);
+  inline Buffer hexStringToBuffer(const char *input, std::size_t inputLength, Buffer output) {
+    if(inputLength == 0) return nullptr;
+    if(inputLength % 2 == 1) *(output++) = hexCharToByte(*(input++));
+    while(*input != '\0') {
+      *(output++) = 16 * hexCharToByte(*input) + hexCharToByte(*(input + 1));
+      input += 2;
+    }
+    return output - 1;
+  }
 
   /**
    * @brief Converts hexadecimal string to buffer
    * 
    * @param input null-terminated c string 
    * @param output output buffer
-   * @return pointer to the last byte
+   * @return Pointer to the last byte
    */
-  Buffer hexStringToBuffer(const char *input, Buffer output);
+  inline Buffer hexStringToBuffer(const char *input, Buffer output) {
+    return hexStringToBuffer(input, strlen(input), output);
+  }
 
   /**
    * @brief 
@@ -49,9 +68,24 @@ namespace Utils {
    * @param inputLength length of the input buffer 
    * @param output output string, padded to even length
    * @param nullTerminated should string be null terminated, defaults to false
-   * @return pointer to the last char, nullptr if buffer is empty
+   * @return Pointer to the last char, nullptr if buffer is empty
    */
-  char *bufferToHexString(Buffer input, size_t inputLength, char *output, bool nullTerminated = false);
+  inline char *bufferToHexString(Buffer input, size_t inputLength, char *output, bool nullTerminated = false) {
+    if(inputLength == 0) return nullptr;
+    Buffer inputEnd = input + inputLength;
+    while(input != inputEnd) {
+      *output = byteToHexChar((*input / 16) % 16);
+      *(output + 1) = byteToHexChar(*input % 16);
+
+      ++input;
+      output += 2;
+    }
+
+    if(nullTerminated) {
+      *output = '\0';
+      return output;
+    } else return output - 1;
+  }
 
   /**
    * @brief Get the most significant non zero byte
@@ -59,14 +93,38 @@ namespace Utils {
    * @param x input integer
    * @return Byte 
    */
-  Byte getMostSignificantNonZeroByte(std::uint64_t x);
+  inline Byte getMostSignificantNonZeroByte(std::uint64_t x) {
+    if(x == 0) return 0;
+
+    Byte MSB;
+    std::uint_fast8_t n = 7;
+    while((MSB = (x >> 8 * (n--)) & 0xFF) == 0);
+
+    return MSB;
+  }
 
   /**
    * @brief Convert integer to buffer
    * 
    * @param x input integer, requires 8 bytes
    * @param output output buffer
-   * @return pointer to the last byte 
+   * @return Pointer to the last byte 
    */
-  Buffer intToBuffer(std::uint64_t x, Buffer output);
+  inline Buffer intToBuffer(std::uint64_t x, Buffer output) {
+    Buffer outputStart = output + 8;
+    Buffer outputEnd = outputStart;
+
+    if(x == 0) {
+      *output = 0;
+      return output;
+    }
+
+    for(; x != 0; x >>= 8) *(--outputStart) = x & 0xFF;
+    std::size_t length = outputEnd - outputStart;
+
+    // Move elements to the beginning of buffer
+    memmove(output, outputStart, length);
+
+    return output + length - 1;
+  }
 }
